47eefec275b95e161a1d4ff9da606ebb547d5c95
Jamie M Dowden-Duarte (Formally Gavina) <152818382+Goober0415@users.noreply.github.com>
2024-11-12 14:54:48 -0700

    11/12/2024 code day 2
    Adafruit feeds, SEEED sensors, bme, display

--- SelfWateringPlant.cpp
+++ SelfWateringPlant.cpp
@@ -22,10 +22,22 @@ SYSTEM_MODE(AUTOMATIC);
 TCPClient TheClient;
 
 // Adafruit.io feeds
+Adafruit_MQTT_SPARK mqtt(&TheClient, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+// subscription
+Adafruit_MQTT_Subscribe waterFeed = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "feeds/waterFeed");
+// publish
+Adafruit_MQTT_Publish airQualFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "feeds/airQualFeed");
+Adafruit_MQTT_Publish humFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "feeds/humFeed");
+Adafruit_MQTT_Publish moistureFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "feeds/mostureFeed");
+Adafruit_MQTT_Publish tempFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "feeds/tempFeed");
 
 // constants
 const int OLED_RESET = -1;
+const int PUMP = D16;
 const int HEXADDRESS = 0x76;
+const int DHEXADDRESS = 0x3C;
+const int DUSTS = A0;
+const int HOUR = 600000;
 
 // class
 String dateTime, timeOnly;
@@ -33,15 +45,21 @@ String dateTime, timeOnly;
 // objects
 Adafruit_SSD1306 display(OLED_RESET);
 Adafruit_BME280 bme;
+AirQualitySensor airQualSens(A1);
 
 // Variables
-unsigned int startTime1, airValue, duration, lowPulseOcc, currentQuality = -1;
+unsigned int startTime1, airValue, duration, lowPulse, currentQual = -1;
 int soilMoist = A2, subValue, tempC, presPA, humRH, tempF, inHG;
-float dielectric;
+float ratio = 0, concentration = 0;
 bool status;
 
 // functions
+void MQTT_connect();
 void bmeReads(int timeFrame);
+void dustS(int timeFrame);
+void airS(int timeFrame);
+void soilReads(int pin, int timeFrame, int threshold);
+void waterPump(int pump, int timeON);
 
 void setup()
 {
@@ -59,23 +77,58 @@ void setup()
   }
   Time.zone(-7);
   Particle.syncTime();
-  display.begin(SSD1306_SWITCHAPVCC, Ox3C);
+  display.begin(SSD1306_SWITCHCAPVCC, DHEXADDRESS);
   display.display();
   delay(2000);
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  MQTT.connect();
 
-  // subscriptions
+  // subscription
+  mqtt.subscribe(&waterFeed);
 
   // pinMode
   pinMode(soilMoist, INPUT);
+  digitalWrite(PUMP, subValue);
 }
 
 void loop()
 {
+  MQTT_connect();
   bmeReads(10000);
+  dustS(HOUR);
+  airS(10000);
+  soilReads(soilMoist, HOUR, 2700);
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(100)))
+  {
+    if (subscription == &waterFeed)
+    {
+      subValue = atof((char *)waterFeed.lastread);
+      digitalWrite(PUMP, subValue);
+      Serial.printf("Water the plant: %i\n", subValue);
+    }
+  }
+}
+
+void MQTT_connect()
+{
+  int8_t ret;
+
+  // Return if already connected.
+  if (mqtt.connected())
+  {
+    return;
+  }
+  Serial.printf("MQTT connected!\n");
+  while ((ret = mqtt.connect()) != 0)
+  { // connect will return 0 for connected
+    Serial.printf("Error Code %s\n", mqtt.connectErrorString(ret));
+    Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+    mqtt.disconnect();
+    delay(5000); // wait 5 seconds and try again
+  }
+  Serial.printf("MQTT Connected!\n");
 }
 
 void bmeReads(int timeFrame)
@@ -88,7 +141,7 @@ void bmeReads(int timeFrame)
     lastTime = millis();
     display.clearDisplay();
     tempC = bme.readTemperature();
-    presPA = bme.readPresure();
+    presPA = bme.readPressure();
     humRH = bme.readHumidity();
 
     tempF = map(tempC, 0, 38, 32, 100);
@@ -106,4 +159,92 @@ void bmeReads(int timeFrame)
     humFeed.publish(humRH);
     tempFeed.publish(tempF);
   }
+}
+
+void dustS(int timeFrame)
+{
+  static unsigned int currentTime, lastTime;
+  int startTime = millis();
+  currentTime = millis();
+  if ((currentTime - lastTime) > timeFrame)
+  {
+    lastTime = millis();
+    duration = pulseIn(DUSTS, LOW);
+    lowPulse = lowPulse + duration;
+  }
+  if ((millis() - startTime) > 30000)
+  {
+    ratio = lowPulse / (30000.0);
+    concentration = 1.1 * pow(ratio, 3) - 3.8 * pow(ratio, 3) + 520 * ratio + 0.62;
+    Serial.printf("Low pulse = %i, Ratio = %f\n", lowPulse, ratio);
+    lowPulse = 0;
+    startTime = millis();
+  }
+  return;
+}
+
+void airS(int timeFrame)
+{
+  static unsigned int currentTime, lastTime;
+  currentTime = millis();
+
+  if ((currentTime - lastTime))
+  {
+    lastTime = millis();
+    airValue = airQualSens.getValue();
+    currentQual = airQualSens.slope();
+    switch (currentQual)
+    {
+    case 0:
+      Serial.printf("Caution! high pollution!\n");
+      airQualFeed.publish("HIGH POLLUTION DETECTED, OH NO! WE'RE GONNA DIE!\n");
+      break;
+
+    case 1:
+      Serial.printf("Pollution rising!\n");
+      airQualFeed.publish("Pollution rising!\n");
+      break;
+
+    case 2:
+      Serial.printf("Low pollution.\n");
+      airQualFeed.publish("Low pollution\n");
+      break;
+
+    case 3:
+      Serial.printf("Ahh, fresh air!\n");
+      airQualFeed.publish("Ahh, fresh airt!\n");
+      break;
+    }
+    Serial.printf("Quant Value = %i\n", airValue);
+  }
+}
+
+void soilReads(int pin, int timeFrame, int threshold)
+{
+  static unsigned int currentTime, lastTime;
+  int moisturereads;
+
+  currentTime = millis();
+  moisturereads = analogRead(pin);
+
+  if ((currentTime - lastTime) > timeFrame)
+  {
+    lastTime - millis();
+    moistureFeed.publish(moisturereads);
+    if (moisturereads > timeFrame)
+    {
+      Serial.printf("soil is dry! Watering pump activated");
+    }
+    else
+    {
+      Serial.printf("Soil moisture is at the right levels");
+    }
+  }
+}
+
+void waterPump(int pump, int timeON)
+{
+  digitalWrite(pump, HIGH);
+  delay(timeON);
+  digitalWrite(pump, LOW);
 }
